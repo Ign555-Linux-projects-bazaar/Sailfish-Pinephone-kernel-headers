9c8d094d07aa1198b665fec49e227629d9b51545
Ingeno <88973001+Ign555@users.noreply.github.com>
2026-05-20 00:11:19 +0200

    edit installation script

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
 echo "Installing kernel headers..."
 
-if [ ! -d "/usr/local/include" ]
+if [ ! -d "/usr/local/linux-7.0.0-pgz-pine64" ]
 then
-	mkdir /usr/local/include
+	mkdir /usr/src/linux-7.0.0-pgz-pine64
 fi
 
-cp -v -r ./headers/* /usr/local/include
+cp -v -r ./headers/* /usr/src/linux-7.0.0-pgz-pine64
 
 echo "done"
 
+echo "Create link"
+
+ln -s /usr/src/linux-7.0.0-pgz-pine64 /lib/modules/7.0.0-pgz-pine64/build
